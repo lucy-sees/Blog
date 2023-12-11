febc9ef351f11e1e60fa5cfc59ce836c7442181a
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-12-11 18:40:37 +0300

    Run rails g controller to generate users and posts controllers

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end 
-
+  resources :posts, only: [:index, :show]
   root 'users#index'
 end
