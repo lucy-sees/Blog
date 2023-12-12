f7f0f62f3564dcc18765c84e0963329ead5fb88f
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-12-12 10:49:03 +0300

    Update application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
-  def gravatar_for(user, size: 80, default_url: 'https://example.com/default_avatar.png')
-    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=#{CGI.escape(default_url)}"
+  def gravatar_for(user, default_url: 'https://gravatar-profile-pictures.s3.eu-north-1.amazonaws.com/BIT.png')
+    default_url
   end
 end
