a0e2b5400c6b60ec14fbecab489c7ce055be4fda
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-12-16 14:15:28 +0300

    Remove current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    @current_user ||= User.first
-    before_action :authenticate_user!
-    before_action :configure_permitted_parameters, if: :devise_controller?
-  end
+  # def current_user
+  #   @current_user ||= User.first
+  #   before_action :authenticate_user!
+  #   before_action :configure_permitted_parameters, if: :devise_controller?
+  # end
 
   protected
 
